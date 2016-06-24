d0c6a33edad3d4d2ec2aa82e9957e8504be9251b
tafdev <chombot@hotmail.com>
2016-06-24 16:08:03 +0000

    fixing up the create and flash errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,16 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article Created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @artcile = Article.find(params[:id])
   end
   
   private
