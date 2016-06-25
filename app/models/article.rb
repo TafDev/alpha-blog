3f7acda2a3a2f1f3b062a1f39f9e808fef92f4c9
tafdev <chombot@hotmail.com>
2016-06-25 13:02:45 +0000

    add associations and debugger

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title, :desription, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625113655) do
+ActiveRecord::Schema.define(version: 20160625121750) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "desription"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
