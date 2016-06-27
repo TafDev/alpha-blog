4bffb7bd7ef4ad76f9d1a2cf275c11146083e843
tafdev <chombot@hotmail.com>
2016-06-27 22:08:10 +0000

    added admin priviledges

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,7 +56,7 @@ class ArticlesController < ApplicationController
   end
   
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin?
       flash[:danger] = "you can only edit or delete you own article"
       redirect_to root_path
     end
